28e2d122bdc046f21c0e337b3b04766bf68735f3
Angelo F <angelo.l.fallaria@gmail.com>
2021-07-06 01:30:06 +0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,145 +1,87 @@
 #! python3
-# hangman.py - Guess numbers before you get hanged!
+'''
+madlibs.py - Replace ADJECTIVE, NOUN, ADVERB and VERB
+             in text files then save them to a new file.
 
-import random
-import sys
+Usage: Create a \text folder in the directory and
+       put in .txt files with constructions like
+       ADJECTIVE, NOUN, ADVERB and VERB to be replaced.
+'''
 
-pics = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-"""
-Hangman ASCII from
-https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
-"""
-
-# Get the list of words from hangman_words.txt from the same directory.
-hangman_words = open('.\\hangman_words.txt').readlines()
-
-
-def main_game():
-    print('--HANGMAN--')
-
-    word = random.choice(hangman_words).strip('\n')
-    solved = [' '] * len(word)
-
-    # Counter of wrong guesses
-    counter = 0
-
-    # Number of wrong guesses available before game over
-    limit = 7 - 1
-
-    # Letters that are not in the word
-    wrong_guesses = []
-
-    while True:
-
-        # Print hangman
-        print(pics[counter])
-
-        # Print the word
-        print('Word: ', end='')
-        for i in solved:
-            if i == ' ':
-                print('_ ', end='')
-            else:
-                print(f'{i} ', end='')
-        print('')
-
-        # Print wrong guesses
-        print(f"Misses: ", end='')
-        print(*wrong_guesses, sep=', ')
-
-        # Check win condition
-        if ''.join(solved) == word:
-            print('You won!\n')
-            break
-        elif counter >= limit:
-            print(f"Game over!\nThe word was '{word}'\n")
-            break
-
-        # Input a letter
-        while True:
-            try:
-                letter = input('>> ')[0].lower()
-            except IndexError:
-                continue
-
-            # Player cannot input in already wrong guesses
-            if letter in wrong_guesses:
-                continue
-
-            break
-
-        # Check if letter is in word
-        if letter in word:
-
-            # Reveal the correct letter in solved
-            for i in range(len(word)):
-                if word[i] == letter:
-                    solved[i] = word[i]
-
-        # If not, add to hangman counter
-        else:
-            counter += 1
-            wrong_guesses.append(letter)
+import os
+import re
 
 
 def __main__():
-    while True:
-        print("Wanna play hangman? Enter 'y'! ('q' to quit)")
-        user_input = input('>> ')
 
-        if user_input.lower() == 'y':
-            main_game()
-        elif user_input.lower() == 'q':
-            sys.exit()
+    if not os.path.exists('.\\text'):
+        print("No text folder 'text' detected.\
+               Create one to do Mad Libs in there.")
+        return None
+
+    # Get files in text folder.
+    files = os.listdir('.\\text')
+
+    # Remove non .txt files and already madlibbed files
+    for file in files:
+        if file[-4:] != '.txt' or file[-14:] == '_madlibbed.txt':
+            files.remove(file)
+
+    # List files available to select.
+    print('Select a file to do MAD LIBS in.')
+    for file in range(len(files)):
+        print(f'{file + 1}. {files[file]}')
+
+    # Ask for file input from user.
+    while True:
+        try:
+            select_file = int(input('>> '))
+            current_file = files[select_file - 1]
+            break
+
+        except IndexError:
+            print('Invalid input. Try again.')
+            continue
+
+        except ValueError:
+            print('Invalid input. Try typing in the number of the text file\
+                   you want to edit.')
+            continue
+
+    print(f'\n{current_file}:')
+    ml_input = open(f'.\\text\\{current_file}').read()
+    print(f'{ml_input}\n')
+
+    # Find occurences of ADJECTIVE, NOUN, ADVERB and VERB.
+    regex_words = re.compile(r'(ADJECTIVE|NOUN|ADVERB|VERB)+')
+    words = regex_words.findall(ml_input)
+
+    # Input the user to replace the words.
+
+    replacers = []
+
+    for word in words:
+
+        # Enter a <grammatical structure>
+        if word[0] == 'A':
+            print('Enter an ', end='')
+        else:
+            print('Enter a ', end='')
+
+        print(f'{word.lower()}:')
+        replacers.append(input('>> '))
+
+    # Replace them.
+    for i in range(len(replacers)):
+        ml_input = ml_input.replace(words[i], replacers[i], 1)
+        # Print results to screen.
+    print(f'\nSaved to {current_file[:-4]}_madlibbed.txt:')
+    print(ml_input)
+
+    # Save results to new text file.
+    ml_output = open(f'.\\text\\{current_file[:-4]}_madlibbed.txt', 'w')
+    ml_output.write(ml_input)
+    ml_output.close()
 
 
 __main__()
